9420764cbf3b45643f94d17f195595c1de16354d
emelymack <emelymack@gmail.com>
2023-06-23 21:26:21 -0300

    Refactored navigation-mobile.test

--- src/features/navigation/mobile/navigation-mobile.test.tsx
+++ src/features/navigation/mobile/navigation-mobile.test.tsx
@@ -6,42 +6,40 @@ import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import { store } from "store/store";
 
-describe("Mobile navigation component", () => {
+const linksText = ['Home', 'Following']
+describe("NavMobile component", () => {
   beforeEach(() => {
-    window.innerWidth = 400;
-    window.innerHeight = 500;
     act(() => {
-      window.dispatchEvent(new Event('resize'))
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <LanguageProvider>
+              <NavMobile />
+            </LanguageProvider>
+          </BrowserRouter>
+        </Provider>
+      );
     })
-
-    render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <LanguageProvider>
-            <NavMobile />
-          </LanguageProvider>
-        </BrowserRouter>
-      </Provider>
-    );
 
     const hamburgerBtn = screen.getByRole('button')
     expect(hamburgerBtn).toBeInTheDocument()
   })
 
   it("Should not render menu at the beginning", () =>{ 
-    expect(screen.queryByText('Following')).not.toBeInTheDocument()
+    linksText.forEach((link) => {
+      expect(screen.queryByText(link)).not.toBeInTheDocument()
+    })
   })
 
   it("Should render menu on clicking hambuger menu", async () => {
     const hamburgerBtn = screen.getByRole('button')
-    
-    act(() => { userEvent.click(hamburgerBtn) })
 
-    const HomeLink = await screen.findByText('Home')
-    const FollowingLink = await screen.findByText('Following')
+    act(() => { userEvent.click(hamburgerBtn) })
     const searchInput = await screen.findByPlaceholderText('Search...')
-    expect(HomeLink).toBeInTheDocument()
-    expect(FollowingLink).toBeInTheDocument()
+
+    linksText.forEach((link) => {
+      expect(screen.queryByText(link)).toBeInTheDocument()
+    })
     expect(searchInput).toBeVisible()
   })
 })
